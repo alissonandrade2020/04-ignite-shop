25064c62871c1131b17235528966b2b6dcd0a208
Alisson de Andrade Araujo <alisson.araujo@sefaz.pb.gov.br>
2023-10-06 10:03:43 -0300

    Shop - Ignite - .Env - url

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -68,7 +68,7 @@ export default function Product({ product }: ProductProps) {
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [
-      { params: { id: 'prod_MLH5Wy0Y97hDAC' } },
+      { params: { id: 'prod_OlkAhQ0b0kNxTa' } },
     ],
     fallback: 'blocking',
   }
